fd1f6b36e22aeab51b3d0ebafad97785953abfb6
mo1der <mo1der@hotmail.com>
2022-12-05 11:11:46 +0100

    fg

---
+++ d24_project/main.py
@@ -0,0 +1,11 @@
+# file = open("my_file.txt")
+# tresc = file.read()
+# print(tresc)
+
+# with open("my_file.txt") as file:
+#     tresc = file.read()
+#     print(tresc)
+
+
+with open("mo1der2.txt", mode="w") as file:
+    file.write("\n this is sample mo1der")
